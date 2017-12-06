9b135e49b545cd000022abf4f9c487165bd0c53c
Anton Semjonov <anton@semjonov.de>
2017-12-06 02:08:06 +0100

    add simple argument parsing

--- keyconvert.py
+++ keyconvert.py
@@ -1,9 +1,16 @@
+#!/usr/bin/env python3
+
 """Convert armored OpenSSH key to binary TinySSH format."""
 
 from readkey import readkey
 from key import SecKey
-from json import dumps as json
 
-key = SecKey(readkey('./testkey'))
+from argparse import ArgumentParser
+
+A = ArgumentParser()
+A.add_argument('-k', '--key', help='OpenSSH private key', required=True)
+args = A.parse_args()
+
+key = SecKey(readkey(args.key))
 
 print(key.toJSON())
