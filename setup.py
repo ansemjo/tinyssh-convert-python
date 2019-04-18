b039f1a392f17777d7a02b89f5214b2f0a1dadb3
Anton Semjonov <anton@semjonov.de>
2019-04-18 12:22:35 +0200

    embed version in package and display in usage epilogue

--- setup.py
+++ setup.py
@@ -4,11 +4,16 @@
 from subprocess import check_output as cmd
 from setuptools import setup, find_packages
 
+# embed version in package
+version = cmd(["sh", "./version.sh", "version"]).strip().decode()
+with open("tinyssh_keyconvert/__version__.py", "w") as versionfile:
+    versionfile.write("__version__ = \"%s\"" % version)
+
 setup(
     name="tinyssh-keyconvert",
     description="Convert OpenSSH ed5519/ecdsa-nistp256 secretkeys to binary format for TinySSH.",
     keywords="openssh tiyssh privatekey key converter",
-    version=cmd(["sh", "./version.sh", "version"]).strip().decode(),
+    version=version,
     license="GPL-3.0",
     author="Anton Semjonov",
     author_email="anton@semjonov.de",
